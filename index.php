6f15059c6260ac2eb973312caf8e161646d9a64d
unknown <Administrador@.(none)>
2010-07-29 12:27:12 -0400

    New repo

---
+++ index.php
@@ -0,0 +1,29 @@
+
+
+
+
+<html>
+  <head>
+  <title>Arena</title>
+  </head>
+  
+  <LINK rel="stylesheet" type="text/css" href="css/default.css">
+  <LINK rel="stylesheet" type="text/css" href="css/arena.css">
+  <body>
+ 
+    <div id="nav">
+    </div>
+    
+    <div id="marco">
+      <?php
+        include("system/arena.php");
+      ?>
+    </div>
+    
+    <div id="footer">
+    </div>  
+    
+  </body>
+
+
+</html>
---
+++ system/arena.php
@@ -0,0 +1,19 @@
+<div id="arena">
+
+  <div id="enemy">
+    <div id="enemy_card1"><img src="resources/cards/E01N02.PNG" width="160"/></div>
+    <div id="enemy_card2"><img src="resources/cards/E01N01.PNG" width="160"/></div>
+    <div id="enemy_card3"><img src="resources/cards/E01N02.PNG" width="160"/></div>
+    <div id="enemy_card4"><img src="resources/cards/E01N01.PNG" width="160"/></div>
+    <div id="enemy_card5"><img src="resources/cards/E01N02.PNG" width="160"/></div>
+  </div>
+  
+  <div id="player">
+    <div id="player_card1"><img src="resources/cards/E01N01.PNG" width="160"/></div>
+    <div id="player_card2"><img src="resources/cards/E01N02.PNG" width="160"/></div>
+    <div id="player_card3"><img src="resources/cards/E01N01.PNG" width="160"/></div>
+    <div id="player_card4"><img src="resources/cards/E01N02.PNG" width="160"/></div>
+    <div id="player_card5"><img src="resources/cards/E01N01.PNG" width="160"/></div>  
+  </div>
+
+</div>
